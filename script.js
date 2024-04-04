08db17f59c308c6f83552046f0a0b462179d2824
jagdishbhatt07 <jagadishbhatt817@gmail.com>
2024-04-04 19:08:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// function changeColor(color) {
+//     var colorBox = document.getElementById('colorBox');
+//     colorBox.style.backgroundColor = color;
+//   }
+
+const changeColor = color => {
+  const colorBox = document.getElementById('colorBox');
+  colorBox.style.backgroundColor= color;
+};
+
+  
+
